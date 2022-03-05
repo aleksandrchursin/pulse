5427dfdaef3b2240d6bb42a2361f7396675f2006
Aleksand <achursin7@gmail.com>
2022-03-05 14:00:34 +0300

    third

--- src/js/script.js
+++ src/js/script.js
@@ -1,99 +1,72 @@
-//TODO: Refactor to a module pattern for the whole slider
+var name = "Aleksandr";
 
-$(function () {
-    var width = 600;
-    var aniSpeed = 1000;
-    var delay = 3000;
-    
-    var $slider = $('.slider ul');
+let number = 7;
+const pi = 3.14;
 
+nubmer = 4;
 
-    var myInt;
-    
-    var $slides = $slider.find('li');
-    var slideIndex = 0;
-    
-    var first = $slider.find('li:first').clone();
-    var last = $slider.find('li:last').clone();
-    first.appendTo($slider);    
-    last.prependTo($slider);
-    $slider.css({'margin-left': -width});
-    
-    function createTracker() {
-        $('.slider').after("<div id='tracker'></div>");
-        for(i = 0; i < $slides.length; i++)
-        {
-            $('#tracker').append("<div class='slide-tracker-" + i + "'></div>");
-        }
-        
-        $('.slide-tracker-0').addClass('active');
-    }
-    
-    function createButtons(){
-        $('.slider').append("<div id='leftButton'><</div>");
-        $('.slider').append("<div id='rightButton'>></div>");
-        $('#leftButton').on('click', shiftLeft);
-        $('#rightButton').on('click', shiftRight);
-    }
-    
-    //TODO: DRY this
-    function shiftLeft() {
-        $slider.animate({'margin-left': '+=' + width}, aniSpeed, function(){
-            slideIndex--;
-            $('.active').removeClass('active');
-            $('.slide-tracker-' + slideIndex).addClass('active');
-            
-            if(slideIndex === -1) {
-                $slider.css({'margin-left': (-width * $slides.length)});
-                slideIndex = $slides.length - 1;
-                $('.slide-tracker-' + slideIndex).addClass('active');
-            }
-        });
-    }
-    
-    function shiftRight() {
-        $slider.animate({'margin-left': '-=' + width}, aniSpeed, function(){
-            slideIndex++;
-            $('.active').removeClass('active');
-            $('.slide-tracker-' + slideIndex).addClass('active');
-            
-            if (slideIndex === $slides.length)
-            {
-                $slider.css({'margin-left': -width});
-                slideIndex = 0;
-                $('.slide-tracker-' + slideIndex).addClass('active');
-            }  
-        });
-    }
-    
-    function startSlider() {
-        myInt = setInterval(function(){ shiftRight(); }, delay);
-    }
-    
-    function stopSlider() {
-        clearInterval(myInt);
-    }
-    
-    function slideToIndex(tracker) {
-        var number = tracker.attr('class').split('-');
-        number = number[number.length - 1];
-        
-        $slider.animate({'margin-left': '+=' + width * (slideIndex - number)}, 1000);
-        $('.active').removeClass('active');
-        tracker.addClass('active');
-        slideIndex = number;
-    }
-    
-    createTracker();
-    createButtons();
+let leftBorderWidth = 200;
 
-    //TODO: clean up events, this is lazy
-    $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
-    $('#leftButton').on('mouseenter', stopSlider);
-    $('#rightButton').on('mouseenter', stopSlider);
-    $('[class^=slide-tracker-]').on('mouseenter', stopSlider).on('mouseleave', startSlider);;
-    
-    $('[class^=slide-tracker-]').click(function(){ slideToIndex($(this)); });
-    
-    startSlider();
-});
+//number - числа, можем использовать для обозначения ЗП, кол-ва пикселей, любое кол-во
+// string - строк, любая информация, которую можно описать словами, используется с ковычками "", '', 
+//true/false - условия
+//null 
+//undefined
+// let obj = {
+//     name: 'apple',
+//     color: 'green',
+//     weight: 200
+// }
+//Symbol - техническая информация
+
+// alert (1234)
+// console.log(nubmer);
+// let answer =confirm("Вам есть 18?");
+// console.log(answer);
+
+// let answer =prompt("Вам есть 18?", "");
+// console.log(answer);
+
+// console.log(4 + 'fdd');
+
+/* let isChecked = false,
+    isClose = false; */
+
+// console.log(isChecked && isClose);
+
+/* console.log(isChecked || isClose); */
+
+// if (2*6 == 8*1) {
+//     console.log('Верно')
+// } else {
+//     console.log('Ошибка')
+// }
+
+/* let answer =confirm("Вам есть 18?");
+if (answer) {
+    console.log ('Проходите')
+} else{
+    console.log('Ухади')
+} */
+
+// const num = 50;
+
+// if (num<49) {
+//     console.log('Неправильно')
+// }else if (num > 100) {
+//     console.log('Дофига')
+// }else{
+//     console.log('Чётко')
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(i);
+// }
+
+function logging(a, b) {
+    console.log(a + b)
+}
+
+logging(3, 5);
+
+logging(6, 8);
