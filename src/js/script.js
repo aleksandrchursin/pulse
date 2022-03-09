f05447cba32651c34e8b5f1e8ad5297d19093a36
Aleksand <achursin7@gmail.com>
2022-03-09 11:32:33 +0300

    1'st sliders

--- src/js/script.js
+++ src/js/script.js
@@ -1,72 +1,17 @@
-var name = "Aleksandr";
-
-let number = 7;
-const pi = 3.14;
-
-nubmer = 4;
-
-let leftBorderWidth = 200;
-
-//number - числа, можем использовать для обозначения ЗП, кол-ва пикселей, любое кол-во
-// string - строк, любая информация, которую можно описать словами, используется с ковычками "", '', 
-//true/false - условия
-//null 
-//undefined
-// let obj = {
-//     name: 'apple',
-//     color: 'green',
-//     weight: 200
-// }
-//Symbol - техническая информация
-
-// alert (1234)
-// console.log(nubmer);
-// let answer =confirm("Вам есть 18?");
-// console.log(answer);
-
-// let answer =prompt("Вам есть 18?", "");
-// console.log(answer);
-
-// console.log(4 + 'fdd');
-
-/* let isChecked = false,
-    isClose = false; */
-
-// console.log(isChecked && isClose);
-
-/* console.log(isChecked || isClose); */
-
-// if (2*6 == 8*1) {
-//     console.log('Верно')
-// } else {
-//     console.log('Ошибка')
-// }
-
-/* let answer =confirm("Вам есть 18?");
-if (answer) {
-    console.log ('Проходите')
-} else{
-    console.log('Ухади')
-} */
-
-// const num = 50;
-
-// if (num<49) {
-//     console.log('Неправильно')
-// }else if (num > 100) {
-//     console.log('Дофига')
-// }else{
-//     console.log('Чётко')
-// }
-
-// for (let i = 1; i < 8; i++) {
-//     console.log(i);
-// }
-
-function logging(a, b) {
-    console.log(a + b)
-}
-
-logging(3, 5);
-
-logging(6, 8);
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/section2/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/section2/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+      });
+  });
